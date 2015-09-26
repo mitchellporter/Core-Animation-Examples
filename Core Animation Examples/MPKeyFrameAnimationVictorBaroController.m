ebfb1b6c642b62f98ce59a0291b58d97e5c8ae57
Mitchell Porter <mitchellporter@gmail.com>
2015-09-25 22:59:57 -0700

    Created MPKeyFrameAnimationVictorBaroController class

---
+++ Core Animation Examples/MPKeyFrameAnimationVictorBaroController.m
@@ -0,0 +1,23 @@
+//
+//  MPKeyFrameAnimationVictorBaroController.m
+//  Core Animation Examples
+//
+//  Created by Mitchell Porter on 9/25/15.
+//  Copyright © 2015 Mitchell Porter. All rights reserved.
+//
+
+#import "MPKeyFrameAnimationVictorBaroController.h"
+
+@interface MPKeyFrameAnimationVictorBaroController ()
+
+@end
+
+@implementation MPKeyFrameAnimationVictorBaroController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+@end
