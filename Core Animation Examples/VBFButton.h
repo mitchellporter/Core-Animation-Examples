f59fac11e4f247101b7c86fdbe70d6c9afb04bcb
Mitchell Porter <mitchellporter@gmail.com>
2015-09-25 22:00:28 -0700

    Created VBFButton class

---
+++ Core Animation Examples/VBFButton.h
@@ -0,0 +1,13 @@
+//
+//  VBFButton.h
+//  Core Animation Examples
+//
+//  Created by Mitchell Porter on 9/25/15.
+//  Copyright © 2015 Mitchell Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBFButton : UIView
+
+@end
---
+++ Core Animation Examples/VBFButton.m
@@ -0,0 +1,22 @@
+//
+//  VBFButton.m
+//  Core Animation Examples
+//
+//  Created by Mitchell Porter on 9/25/15.
+//  Copyright © 2015 Mitchell Porter. All rights reserved.
+//
+
+#import "VBFButton.h"
+#import <QuartzCore/QuartzCore.h>
+
+@implementation VBFButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
