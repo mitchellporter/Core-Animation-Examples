b8c5a2b893e0893d108ecfb3aea0e25d02efdba6
Mitchell Porter <mitchellporter@gmail.com>
2015-09-26 14:25:54 -0700

    Created MPLineBounceVictorBaroController class

---
+++ Core Animation Examples/MPLineBounceVictorBaroController.h
@@ -0,0 +1,13 @@
+//
+//  MPLineBounceVictorBaroController.h
+//  Core Animation Examples
+//
+//  Created by Mitchell Porter on 9/26/15.
+//  Copyright © 2015 Mitchell Porter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MPLineBounceVictorBaroController : UIViewController
+
+@end
---
+++ Core Animation Examples/MPLineBounceVictorBaroController.m
@@ -0,0 +1,24 @@
+//
+//  MPLineBounceVictorBaroController.m
+//  Core Animation Examples
+//
+//  Created by Mitchell Porter on 9/26/15.
+//  Copyright © 2015 Mitchell Porter. All rights reserved.
+//
+
+#import "MPLineBounceVictorBaroController.h"
+
+@interface MPLineBounceVictorBaroController ()
+
+@end
+
+@implementation MPLineBounceVictorBaroController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+
+@end
