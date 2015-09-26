02155c344502d153ca7894fff41c6cf41480991d
Mitchell Porter <mitchellporter@gmail.com>
2015-09-25 21:05:44 -0700

    table view delegate + datasource methods

--- Core Animation Examples/MPAnimationExamplesController.m
+++ Core Animation Examples/MPAnimationExamplesController.m
@@ -8,16 +8,37 @@
 
 #import "MPAnimationExamplesController.h"
 
-@interface MPAnimationExamplesController ()
+@interface MPAnimationExamplesController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
 
 @end
 
 @implementation MPAnimationExamplesController
 
+#pragma mark - View Controller Lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+}
+
+#pragma mark - Table View
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return nil;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
 }
 
 @end
